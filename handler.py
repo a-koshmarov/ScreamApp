82b0f871c3ec7f4a983b15fabea71c30e0b4f371
a-koshmarov <35034325+a-koshmarov@users.noreply.github.com>
2018-04-15 23:54:10 +0300

    Cleanup
    Added comments and text, moved windows

--- handler.py
+++ handler.py
@@ -2,20 +2,23 @@ import time
 from playsound import playsound
 import cv2
 
-
+# Initializing image for alert
 img = cv2.imread('bif_alert.bmp')
 prev = -1
+
+# Check the current state
 while True:
     with open("out.txt", "r") as f:
         num = int(f.readline().strip())
-        if prev!=num:
+        # If state changes
+        if prev != num:
+            # If state changes from 1 to 0
             if prev == 1:
                 playsound("notscream.mp3")
-                cv2.moveWindow("ALERT", 100, 100)
                 while True:
                     cv2.imshow("ALERT", img)
-
-                    k = cv2.waitKey(1)
+                    cv2.moveWindow("ALERT", 130, 50)
+                    k = cv2.waitKey(0)
                     if k % 256 == 27:
                         cv2.destroyAllWindows()
                         break
--- webcam.py
+++ webcam.py
@@ -2,30 +2,31 @@ import time
 import cv2
 import numpy as np
 
+# Initializing camera, mask and windows
 cam = cv2.VideoCapture(0)
 fgbg = cv2.createBackgroundSubtractorMOG2()
+cv2.namedWindow("Original")
+cv2.moveWindow("Original", 0, 0)
+cv2.namedWindow("Masked")
+cv2.moveWindow("Masked", 640, 0)
+cv2.namedWindow("Histogram")
+cv2.moveWindow("Histogram", 480, 350)
+font = cv2.cv2.FONT_HERSHEY_SIMPLEX
+
+# Initializing counters for b/w ratio
 img_counter = 0
 img_ratio = [0, 0]
 
 while True:
+    # Reading frame from camera and applying the mask
     ret, frame = cam.read()
     img = cv2.flip(frame, 1)
     fmask = fgbg.apply(img)
 
-    cv2.imshow("window", fmask)
-    cv2.imshow("original", img)
-    hi_gray = cv2.calcHist([fmask], [0], None, [3], [0, 256])
+    if not ret:
+        break
 
-    img_counter += 1
-    if (hi_gray[1] + hi_gray[2]) <= hi_gray[0] * 1.5 / 100:
-        # print("No person")
-        img_ratio[0] += 1
-    else:
-        # print("Yes person")
-        img_ratio[1] += 1
-    # print("{} {} {}".format(*hi_gray))
-    # print(sum(hi_gray))
-
+    # 2D histogram for original colored image
     h = np.zeros((300, 256, 3))
 
     bins = np.arange(256).reshape(256, 1)
@@ -40,35 +41,47 @@ while True:
 
     h = np.flipud(h)
 
-    if not ret:
-        break
+    # 1D histogram for masked image
+    hi_gray = cv2.calcHist([fmask], [0], None, [3], [0, 256])
+
+    # Checking whether the 3/5 images were black or white
+    img_counter += 1
+    if (hi_gray[1] + hi_gray[2]) <= hi_gray[0] * 1.5 / 100:
+        # If black
+        cv2.putText(fmask, "No person", (0, 45), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+        img_ratio[0] += 1
+    else:
+        # If white
+        cv2.putText(fmask, "Yes person", (0, 45), font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+        img_ratio[1] += 1
+
+    # print("{} {} {}".format(*hi_gray))
+
+    # Showing frames and histogram
+    cv2.imshow("Original", img)
+    cv2.imshow("Masked", fmask)
+    cv2.imshow("Histogram", h)
+
+    # Writing current state to a file
+    if img_counter == 5:
+        img_counter = 0
+        if img_ratio[0] > img_ratio[1]:
+            with open("out.txt", "w") as f:
+                f.write('0')
+            print("No person")
+        else:
+            with open("out.txt", "w") as f:
+                f.write('1')
+            print("Yes person")
+        img_ratio = [0, 0]
+
     k = cv2.waitKey(1)
 
     if k % 256 == 27:
         # ESC pressed
         print("Escape hit, closing...")
         break
-    cv2.imshow("test", h)
 
-    # print(img_ratio)
-
-    if img_counter == 5:
-        img_counter = 0
-        if img_ratio[0] > img_ratio[1]:
-            # out.seek(0)
-            out = open("out.txt", "w")
-            out.write('0')
-            out.close()
-            print("No person")
-            # print("{} {}".format(*img_ratio))
-        else:
-            # out.seek(0)
-            out = open("out.txt", "w")
-            out.write('1')
-            out.close()
-            print("Yes person")
-            # print("{} {}".format(*img_ratio))
-        img_ratio = [0, 0]
     time.sleep(1)
 
 cam.release()
